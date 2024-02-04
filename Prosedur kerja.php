a227fcb74a3f4122acbb8dc5094ea91cc6ed1ced
christo <christoper0331@gmail.com>
2024-02-04 12:16:41 +0700

    First Upload

---
+++ Prosedur kerja.php
@@ -0,0 +1,11 @@
+<?php
+$nim="E32232198";
+$nama="Christo";
+$prodi="Teknik Komputer";
+echo "Selamat Praktikum";
+echo $nim;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $prodi;
+?>
---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "Christo";
+$umur = 19;
+$alamat = "Jember";
+
+echo $nama;
+echo "<br/>";
+echo $umur;
+echo "<br/>";
+echo $alamat;
+?>
